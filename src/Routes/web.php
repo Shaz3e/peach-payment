f1ed1e9171f3a66e408abcdba96848f8c582b59d
Shahrukh A. Khan <iam@shaz3e.com>
2023-11-25 14:55:00 +0500

    Routes updated

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Shaz3e\PeachPayment\Http\Controllers\User\PeachPaymentController;
 
-Route::middleware(config('peach-payment.middleware'))
-    ->prefix(config('peach-payment.admin_route'))
-    ->name('admin.')
-    ->group(function(){
-    });
+// Route::middleware(config('peach-payment.middleware'))
+//     ->prefix(config('peach-payment.admin_route'))
+//     ->name('admin.')
+//     ->group(function(){
+//     });
 
-Route::get('peach-payment', [PeachPaymentController::class, 'pay'])->name('peach-payment');
+Route::get('peachpayment', [PeachPaymentController::class, 'createTransaction']);
+Route::get('peachpayment/create-transaction', [PeachPaymentController::class, 'postTransaction'])->name('peachpayment.post');
